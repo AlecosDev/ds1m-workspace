3e29e8d096b8d80e63f4aceb64343bc4305e33c1
ALEX VINICIUS COSTA DA SILVA <20272488@CFP127ALUNO.LOCAL>
2021-01-18 11:29:31 -0300

    Aula 18-01-2021

--- Conta1/src/br/senai/sp/jandira/App.java
+++ Conta1/src/br/senai/sp/jandira/App.java
@@ -5,11 +5,22 @@ import br.senai.sp.jandira.model.Conta;
 public class App {
 
 	public static void main(String[] args) {
-
+		Conta contaDaAna = new Conta();
 		Conta contaDaMaria = new Conta();
 		Conta contaDoJoao;
 		contaDoJoao = new Conta();
-
+		
+		contaDaAna.titular = "Ana Gomes";
+		contaDaAna.numero = "001-98";
+		contaDaAna.tipo = "Corrente"; 
+		contaDaAna.ativa = true;
+		contaDaAna.chequeEspecial = 200;
+		contaDaAna.depositar(500);
+		contaDaAna.mostrarSaldoDaConta();
+		contaDaAna.sacar(700);
+		contaDaAna.mostrarSaldoDaConta();
+		contaDaAna.getSaldo();
+		
 		// Determinar os dados da conta da Maria
 
 		contaDaMaria.numero = "100-85";
@@ -38,5 +49,12 @@ public class App {
 
 		contaDaMaria.depositar(-20);
 		contaDaMaria.mostrarSaldoDaConta();
+		
+		int a = somar(10, 12);
+	}
+	
+	public static int somar(int valor1, int valor2) {
+		int resultado = valor1 + valor2;
+		return resultado;
 	}
 }
--- Conta1/src/br/senai/sp/jandira/model/Conta.java
+++ Conta1/src/br/senai/sp/jandira/model/Conta.java
@@ -6,27 +6,42 @@ public class Conta {
 	public String numero;
 	public String titular;
 	public boolean ativa;
-
+	public double chequeEspecial;
+	
 	public void depositar(double valorDeposito) {
 
 		if (valorDeposito > 0) {
 			
-			saldo = saldo + valorDeposito;
+			saldo += valorDeposito;
 			
 		} else {
 			System.out.println("Valor inválido");
 		}
 	}
+	
+	public void sacar(double valorDoSaque) {
 
-	public void mostrarSaldoDaConta() {
-		System.out.println("Saldo: " + saldo);
+		if ( (saldo + chequeEspecial) >= valorDoSaque) {
+			
+			saldo -= valorDoSaque;
+			
+		} else {
+			System.out.println("Saldo insuficiente");
+		}
 	}
 
-	public void sacar() {
-
+	public double getSaldo() {
+		return saldo;
+		
 	}
-
+	
 	public void transferir() {
 
 	}
+	
+	public void mostrarSaldoDaConta() {
+		System.out.println("Saldo: " + saldo);
+		System.out.println("Saldo + limite: " + (saldo + chequeEspecial));
+	}
+
 }
